7fb9bc0e8859f301ef873e5a465b388946d11cc9
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-16 18:26:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,18 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\b[aeiou]\w*/i).length > 0
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+/i).to_a
 end
 
 def words_five_letters_long(text)
+  text.match()
+  
 
 end
 
